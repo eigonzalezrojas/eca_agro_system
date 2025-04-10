f6cf5b2556be9c2a715570a12e0b59a95d7f7f8f
Eithel Gonzalez <eigonzalez@Mac-mini-de-Eithel.local>
2025-04-09 23:52:11 -0400

    modify rut historialClima

--- app/routes/client/datos.py
+++ app/routes/client/datos.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, session, jsonify, request
-from app.models import Registro, Dispositivo, DataP0, HistorialClima
+from app.models import Registro, Dispositivo, DataNodoAmbiente, HistorialClima
 from datetime import datetime, timedelta
 from app.extensions import db
 from sqlalchemy import func
@@ -7,68 +7,60 @@ from sqlalchemy import func
 datos = Blueprint('datos', __name__)
 
 
-@datos.route('/')
+@datos.route('/', methods=['GET'])
 def obtener_datos():
     user_id = session.get('user_id')
 
     if not user_id:
         return jsonify({"error": "Usuario no autenticado"}), 401
 
-    registro = Registro.query.filter_by(fk_usuario=user_id).first()
-    if not registro:
-        return jsonify({"error": "No hay dispositivos asociados al usuario"}), 404
+    chipid = request.args.get('chipid')
 
-    dispositivo = Dispositivo.query.filter_by(id=registro.fk_dispositivo).first()
-    if not dispositivo:
-        return jsonify({"error": "No se encontró el dispositivo asociado"}), 404
+    if not chipid:
+        return jsonify({"error": "Dispositivo no especificado"}), 400
 
-    chipid = dispositivo.chipid
-    ultima_medicion = DataP0.query.filter_by(chipid=chipid).order_by(DataP0.fecha.desc()).first()
+    ultima_medicion = DataNodoAmbiente.query.filter_by(chipid=chipid).order_by(DataNodoAmbiente.fecha.desc()).first()
 
     if not ultima_medicion:
         return jsonify({"error": "No hay mediciones disponibles para el dispositivo"}), 404
 
-    # Obtener fecha de ayer
     fecha_ayer = datetime.now().date() - timedelta(days=1)
 
-    # Buscar si ya hay un registro en HistorialClima
-    historial = HistorialClima.query.filter_by(rut=user_id, chipid=chipid, fecha=fecha_ayer).first()
+    historial = HistorialClima.query.filter_by(chipid=chipid, fecha=fecha_ayer).first()
 
-    # btener el GDA acumulado del día anterior
-    historial_anterior = HistorialClima.query.filter_by(rut=user_id, chipid=chipid, fecha=fecha_ayer - timedelta(days=1)).first()
+    historial_anterior = HistorialClima.query.filter(
+        HistorialClima.chipid == chipid,
+        HistorialClima.fecha < fecha_ayer
+    ).order_by(HistorialClima.fecha.desc()).first()
+
     gda_acumulado_anterior = historial_anterior.gda if historial_anterior else 0
 
     if not historial:
-        # Calcular Temp Max y Temp Min del día anterior
-        temp_max = DataP0.query.filter(
-            DataP0.chipid == chipid,
-            DataP0.fecha.between(fecha_ayer, fecha_ayer + timedelta(days=1))
-        ).order_by(DataP0.temperatura.desc()).first()
+        temp_max = DataNodoAmbiente.query.filter(
+            DataNodoAmbiente.chipid == chipid,
+            DataNodoAmbiente.fecha.between(fecha_ayer, fecha_ayer + timedelta(days=1))
+        ).order_by(DataNodoAmbiente.temperatura.desc()).first()
 
-        temp_min = DataP0.query.filter(
-            DataP0.chipid == chipid,
-            DataP0.fecha.between(fecha_ayer, fecha_ayer + timedelta(days=1))
-        ).order_by(DataP0.temperatura.asc()).first()
+        temp_min = DataNodoAmbiente.query.filter(
+            DataNodoAmbiente.chipid == chipid,
+            DataNodoAmbiente.fecha.between(fecha_ayer, fecha_ayer + timedelta(days=1))
+        ).order_by(DataNodoAmbiente.temperatura.asc()).first()
 
         temp_max = temp_max.temperatura if temp_max else None
         temp_min = temp_min.temperatura if temp_min else None
 
-        # Calcular Horas Frío
-        horas_frio = DataP0.query.filter(
-            DataP0.chipid == chipid,
-            DataP0.fecha.between(fecha_ayer, fecha_ayer + timedelta(days=1)),
-            DataP0.temperatura >= 0,
-            DataP0.temperatura <= 7.2
+        horas_frio = DataNodoAmbiente.query.filter(
+            DataNodoAmbiente.chipid == chipid,
+            DataNodoAmbiente.fecha.between(fecha_ayer, fecha_ayer + timedelta(days=1)),
+            DataNodoAmbiente.temperatura >= 0,
+            DataNodoAmbiente.temperatura <= 7.2
         ).count()
 
-        #Calcular GDA: sumar el GDA diario al acumulado del día anterior
         gda_diario = max(((temp_max + temp_min) / 2) - 10, 0) if temp_max and temp_min else 0
-        gda = gda_acumulado_anterior + gda_diario  # Suma al acumulado
+        gda = gda_acumulado_anterior + gda_diario
 
-        # Guardar en HistorialClima
         if temp_max and temp_min:
             nuevo_registro = HistorialClima(
-                rut=user_id,
                 chipid=chipid,
                 fecha=fecha_ayer,
                 temp_max=temp_max,
@@ -100,46 +92,53 @@ def obtener_resumen():
     if not user_id:
         return jsonify({"error": "Usuario no autenticado"}), 401
 
-    chipid = request.args.get('chipid')
+    registro = Registro.query.filter_by(fk_usuario=user_id).first()
+    if not registro:
+        return jsonify({"error": "No hay dispositivos asociados al usuario"}), 404
+
+    dispositivo = Dispositivo.query.filter_by(id=registro.fk_dispositivo).first()
+    if not dispositivo:
+        return jsonify({"error": "No se encontró el dispositivo asociado"}), 404
+
+    chipid = dispositivo.chipid
     periodo = request.args.get('periodo')
     fecha = request.args.get('fecha')
     mes = request.args.get('mes')
     anio = request.args.get('anio')
 
-    #Validación
     if not chipid:
         return jsonify({"error": "Debe seleccionar un dispositivo"}), 400
 
     query = db.session.query(
-        func.max(DataP0.temperatura).label('temp_max'),
-        func.min(DataP0.temperatura).label('temp_min'),
-        func.max(DataP0.humedad).label('hum_max'),
-        func.min(DataP0.humedad).label('hum_min')
-    ).filter(DataP0.chipid == chipid)
+        func.max(DataNodoAmbiente.temperatura).label('temp_max'),
+        func.min(DataNodoAmbiente.temperatura).label('temp_min'),
+        func.max(DataNodoAmbiente.humedad).label('hum_max'),
+        func.min(DataNodoAmbiente.humedad).label('hum_min')
+    ).filter(DataNodoAmbiente.chipid == chipid)
 
     if periodo == "day" and fecha:
-        query = query.filter(func.date(DataP0.fecha) == fecha)
+        query = query.filter(func.date(DataNodoAmbiente.fecha) == fecha)
 
     elif periodo == "month" and mes and anio:
         query = db.session.query(
-            func.day(DataP0.fecha).label('dia'),
-            func.max(DataP0.temperatura).label('temp_max'),
-            func.min(DataP0.temperatura).label('temp_min'),
-            func.max(DataP0.humedad).label('hum_max'),
-            func.min(DataP0.humedad).label('hum_min')
+            func.day(DataNodoAmbiente.fecha).label('dia'),
+            func.max(DataNodoAmbiente.temperatura).label('temp_max'),
+            func.min(DataNodoAmbiente.temperatura).label('temp_min'),
+            func.max(DataNodoAmbiente.humedad).label('hum_max'),
+            func.min(DataNodoAmbiente.humedad).label('hum_min')
         ).filter(
-            func.year(DataP0.fecha) == anio,
-            func.month(DataP0.fecha) == mes
-        ).group_by(func.day(DataP0.fecha))
+            func.year(DataNodoAmbiente.fecha) == anio,
+            func.month(DataNodoAmbiente.fecha) == mes
+        ).group_by(func.day(DataNodoAmbiente.fecha))
 
     elif periodo == "year" and anio:
         query = db.session.query(
-            func.week(DataP0.fecha).label('semana'),
-            func.max(DataP0.temperatura).label('temp_max'),
-            func.min(DataP0.temperatura).label('temp_min'),
-            func.max(DataP0.humedad).label('hum_max'),
-            func.min(DataP0.humedad).label('hum_min')
-        ).filter(func.year(DataP0.fecha) == anio).group_by(func.week(DataP0.fecha))
+            func.week(DataNodoAmbiente.fecha).label('semana'),
+            func.max(DataNodoAmbiente.temperatura).label('temp_max'),
+            func.min(DataNodoAmbiente.temperatura).label('temp_min'),
+            func.max(DataNodoAmbiente.humedad).label('hum_max'),
+            func.min(DataNodoAmbiente.humedad).label('hum_min')
+        ).filter(func.year(DataNodoAmbiente.fecha) == anio).group_by(func.week(DataNodoAmbiente.fecha))
 
     else:
         return jsonify({"error": "Parámetros inválidos o incompletos"}), 400
